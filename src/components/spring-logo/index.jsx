417f2564660791122d8e8347ce4217997a6639f1
Sanb4ik <sashabelaz7@gmail.com>
2023-12-26 23:38:44 +0300

    added media-query to login page

--- src/components/spring-logo/index.jsx
+++ src/components/spring-logo/index.jsx
@@ -1,7 +1,7 @@
 import './index.css';
 const SpringLogo = () => {
   return (
-    <div>
+    <div className='logo-container'>
       <svg
         xmlns='http://www.w3.org/2000/svg'
         x='0px'
