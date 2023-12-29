ef42131a958f8b73c068cb6ca1940cde2d9b9f20
Sanb4ik <sashabelaz7@gmail.com>
2023-12-29 15:41:05 +0300

    tests

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,25 @@
+// import { jest } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
+import { createUserRequest } from './store/authSlice';
+
+jest.mock('./store/authSlice', () => ({
+  ...jest.requireActual('./store/authSlice'),
+  createUserRequest: jest.fn(),
+}));
+
+describe('auth tests', () => {
+  it('login test success', async () => {
+    await createUserRequest({ userName: 'admin', password: '1234' });
+    expect(createUserRequest).toHaveBeenCalled();
+    expect(createUserRequest).toHaveBeenCalledWith({ userName: 'admin', password: '1234' });
+  });
+
+  it('login test failed', async () => {
+    createUserRequest.mockRejectedValue(new Error('Unauthorized'));
+    try {
+      await createUserRequest({ userName: 'adminnn', password: '1234' });
+    } catch (error) {
+      expect(error.message).toBe('Unauthorized');
+    }
+  });
+});
